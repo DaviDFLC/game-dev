d45ad1eb050357e4742feb96eb4bb4adc26fac03
David Felix <dflcoelho@gmail.com>
2023-09-01 20:05:08 -0400

    game-dev

---
+++ Dawn Forest/Player.gd
@@ -0,0 +1,21 @@
+extends Node2D
+class_name player
+
+# Declare member variables here. Examples:
+var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(a)
+	#Este código vai printar o valor armazenamento em A
+	pass # Replace with function body.
+
+ # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(delta):
+	pass
